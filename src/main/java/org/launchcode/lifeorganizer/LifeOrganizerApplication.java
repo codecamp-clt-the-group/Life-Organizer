8647bcaba70b7bcf2fcca136cda23cf6ac49eae6
Chris Bay <chris@chrisbay.net>
2022-03-07 12:08:28 -0600

    Initial commit

---
+++ src/main/java/org/launchcode/lifeorganizer/LifeOrganizerApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.lifeorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeOrganizerApplication.class, args);
+	}
+
+}
