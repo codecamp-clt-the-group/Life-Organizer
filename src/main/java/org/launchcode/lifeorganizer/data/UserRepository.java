2833ca01fac946b19bd46c8431d2688b2a1f7448
Scott Thoreson <scott.thoreson@live.com>
2022-03-08 10:56:21 -0500

    Updates from the first pull request that can be made at this time.

--- src/main/java/org/launchcode/lifeorganizer/data/UserRepository.java
+++ src/main/java/org/launchcode/lifeorganizer/data/UserRepository.java
@@ -1,9 +1,10 @@
 package org.launchcode.lifeorganizer.data;
 
-
 import org.launchcode.lifeorganizer.models.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User,Integer> {
-
+    User findByUsername(String username);
 }
