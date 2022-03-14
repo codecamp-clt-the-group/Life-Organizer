12fdac50f35e4555e0a13129703722c8f1aecf1b
Scott Thoreson <scott.thoreson@live.com>
2022-03-14 17:40:40 -0400

    Updated the OptionFormDTO and SignupFormDTO to allow the user to see there current information and make changes to it.

--- src/main/java/org/launchcode/lifeorganizer/models/dto/OptionFormDTO.java
+++ src/main/java/org/launchcode/lifeorganizer/models/dto/OptionFormDTO.java
@@ -1,13 +1,7 @@
 package org.launchcode.lifeorganizer.models.dto;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
 public class OptionFormDTO extends SignupFormDTO{
-    @NotNull
-    @NotBlank
-    @Size(min = 6)
+
     private String newPassword;
 
     public String getNewPassword() {
@@ -17,5 +11,6 @@ public class OptionFormDTO extends SignupFormDTO{
     public void setNewPassword(String newPassword) {
         this.newPassword = newPassword;
     }
+
 
 }
