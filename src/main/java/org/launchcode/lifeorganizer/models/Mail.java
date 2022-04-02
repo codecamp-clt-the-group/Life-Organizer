94134fffcb3b1a2882dc37adbb5f58fb16daa3cb
Scott Thoreson <scott.thoreson@live.com>
2022-04-02 15:25:57 -0400

    Contains the field that holds the users email from the forgot page. It also contains the Bean for the JavaMailSender.

---
+++ src/main/java/org/launchcode/lifeorganizer/models/Mail.java
@@ -0,0 +1,35 @@
+package org.launchcode.lifeorganizer.models;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+public class Mail {
+    private String email;
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.mailtrap.io");
+        mailSender.setPort(2525);
+
+        mailSender.setUsername("786bc012ac31cc");
+        mailSender.setPassword("1a72ac15d4728d");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
